18e97feeb575b68038b811c7e6424e146532dee9
Michael Westwood <michael@honcho.agency>
2025-10-14 17:48:40 +0100

    scaffolding

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# Release Notes for Poke
+
+## 1.0.0
+- Initial release
---
+++ src/models/Settings.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace tallowandsons\poke\models;
+
+use Craft;
+use craft\base\Model;
+
+/**
+ * Poke settings
+ */
+class Settings extends Model
+{
+}
